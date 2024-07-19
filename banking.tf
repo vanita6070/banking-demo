50f078a5ce2ee8a0d6bca34d2a29f87e4a89a811
Vanita Roy <49209283+vanita6070@users.noreply.github.com>
2024-07-19 18:05:20 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-2"
 }
 # Creating a VPC
 resource "aws_vpc" "proj-vpc" {
@@ -46,7 +46,7 @@ resource "aws_route_table" "proj-rt" {
 resource "aws_subnet" "proj-subnet" {
  vpc_id = aws_vpc.proj-vpc.id
  cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
+ availability_zone = "us-east-2"
  tags = {
  Name = "subnet1"
  }
@@ -128,7 +128,7 @@ resource "aws_eip" "proj-eip" {
 resource "aws_instance" "Prod-Server" {
  ami = "ami-0ef82eeba2c7a0eeb"
  instance_type = "t2.micro"
- availability_zone = "ap-south-1b"
+ availability_zone = "us-east-2"
  key_name = "Tomcat"
  network_interface {
  device_index = 0
