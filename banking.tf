a91f7b182e990696581782ac181e15dd78a0da54
Vanita Roy <49209283+vanita6070@users.noreply.github.com>
2024-07-19 15:44:22 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -1,148 +1,99 @@
-#Initialize Terraform
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">=4.0"
-    }
-}
-}
-#configure the AWS provider
-provider "aws" {
-  region     = "us-east-2"
-}
-# Create a VPC
-resource "aws_vpc" "mproj-vpc" {
-  cidr_block       = "10.0.0.0/16"
-}
-
-# create an Internet Gateway
-resource "aws_internet_gateway" "proj-ig" {
-  vpc_id = aws_vpc.proj-vpc.id
-  tags = {
-    Name = "gateway1"
-  }
-}
-
-# Setting up the Route Table
-resource "aws_route_table" "proj-rt" {
-  vpc_id = aws_vpc.proj-vpc.id
-  route {
-#pointing to the internet
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.proj-ig.id
-  }
-route {
- ipv6_cidr_block = "::/0"
- gateway_id = aws_internet_gateway.proj-ig.id
-}
-  tags = {
-    Name = "rt1"
-  }
-}
-
-# setting up the Subnet 
-resource "aws_subnet" "proj-subnet" {
-  vpc_id     = aws_vpc.proj-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-2"
-  tags = {
-    Name = "subnet1"
-  }
-}
-
-# Associating the subnet with the route table
-resource "aws_route_table_association" "proj-rt-sub-assoc" {
-subnet_id      = aws_subnet.proj-subnet.id
-route_table_id = aws_route_table.proj-rt.id
-}
-
-#creating security group
-resource "aws_security_group" "proj-sg" { 
-name = "proj-sg" 
-description = "Enable web traffic for the project" 
-vpc_id = aws_vpc.proj-vpc.id 
-ingress { 
-from_port = 0 
-to_port   = 0 
-protocol = "-1" 
-cidr_blocks = ["0.0.0.0/0"] 
+provider "aws" { 
+region = "us-east-2" 
+access_key = "AKIA6GBMFS736D7WFNO6"
+secret_key = "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"
 } 
 
-
-egress { 
-from_port = 0 
-to_port   = 0 
-protocol = "-1" 
-cidr_blocks = ["0.0.0.0/0"] 
-} 
-ingress { 
-description = "HTTPS traffic"
-from_port  = 443
-to_port    = 443
-protocol   = "tcp" 
-cidr_blocks = ["0.0.0.0/0"] 
-} 
-ingress { 
-description = "HTTPS traffic"
-from_port  = 0
-to_port    = 65000
-protocol   = "tcp" 
-cidr_blocks = ["0.0.0.0/0"] 
-} 
-ingress { 
-description = "Allow port 80 inbound"
-from_port  = 80
-to_port    = 80
-protocol   = "tcp" 
-cidr_blocks = ["0.0.0.0/0"] 
-} 
-egress { 
-from_port = 0 
-to_port   = 0 
-protocol = "-1" 
-cidr_blocks = ["0.0.0.0/0"]
-ipv6_cidr_blocks = ["::/0"]
-}
+# Create VPC 
+resource "aws_vpc" "myvpc9" { 
+cidr_block = "10.0.0.0/16" 
+instance_tenancy = "default" 
 tags = { 
-Name = "proj-sg1" 
+Name = "myvpc9" 
+}
+}
+
+# Create Subnet  
+resource "aws_subnet" "mysubnet9" { 
+vpc_id = aws_vpc.myvpc9.id 
+cidr_block = "10.0.1.0/24" 
+tags = { 
+Name = "mysubnet9" 
 } 
 } 
 
-#creating a new network interface
-resource "aws_network_interface" proj-ni" {
-subnet_id - aws_subnet.proj-subnet.id
-private_ips = ["10.0.1.10"]
-security_groups = [aws_security_group.proj-sg.id]
-}
-
-#Attaching an elastic IP to the network interface
-resource "aws_eip" "proj-eip" {
-vpc = true
-network_interface - aws_network_interface.proj-ni.id
-associate_with_private_ip - "10.0.1.10"
-}
-
-# Create an ubuntu EC2 Instance
-resource "aws_instance" "prod-server" {
-  ami  = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
-availability_zone = "us-east-2"
-  key_name = "Tomcat"
-network_interface {
-device_index - 0
-network_interface_id - aws_network_interface.proj-ni.id
-}
-user_data = <<-EOF
-#!/bin/bash
-     sudo apt-get update -y
-EOF
-tags = {
-Name - "prod-server"
+# Internet Gateway 
+resource "aws_internet_gateway" "mygw9" { 
+vpc_id = aws_vpc.myvpc9.id 
+tags = { 
+Name = "mygw9" 
 }
 }
 
-  
+# Route Table 
+resource "aws_route_table" "myrt9" { 
+vpc_id = aws_vpc.myvpc9.id 
+route { 
+cidr_block = "0.0.0.0/0" 
+gateway_id = aws_internet_gateway.mygw9.id 
+} 
+tags = { 
+Name = "myrt9" 
+} 
+}
+
+ 
+
+# Route Table Association 
+resource "aws_route_table_association" "myrta9" { 
+subnet_id = aws_subnet.mysubnet9.id 
+route_table_id = aws_route_table.myrt9.id 
+
+# Security Groups 
+resource "aws_security_group" "mysg9" { 
+name = "mysg9" 
+description = "Allow inbound traffic" 
+vpc_id = aws_vpc.myvpc9.id 
+ingress { 
+description = "HTTP" 
+from_port = 80 
+to_port = 80 
+protocol = "tcp" 
+cidr_blocks = ["0.0.0.0/0"] 
+} 
+
+ingress { 
+description = "SSH" 
+from_port = 22 
+to_port = 22 
+protocol = "tcp" 
+cidr_blocks = ["0.0.0.0/0"] 
+} 
+
+egress { 
+from_port = 0 
+to_port = 0 
+protocol = "-1" 
+cidr_blocks = ["0.0.0.0/0"] 
+ipv6_cidr_blocks = ["::/0"] 
+} 
+tags = { 
+Name = "mysg9" 
+} 
+}
+
+# Create Instance 
+resource "aws_instance" "sainstance9" { 
+ami = "ami-0862be96e41dcbf74" 
+instance_type = "t2.micro" 
+associate_public_ip_address = true 
+subnet_id = aws_subnet.mysubnet9.id 
+vpc_security_group_ids = [aws_security_group.mysg9.id] 
+key_name = "Tomcat" 
+tags = { 
+Name = "SA-TF-TestInstance1" 
+} 
+} 
 
 
  
