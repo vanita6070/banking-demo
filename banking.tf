60fe2980b8f2daf2164b46bbe10652bf11688518
Vanita Roy <49209283+vanita6070@users.noreply.github.com>
2024-07-19 18:26:53 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -125,9 +125,9 @@ resource "aws_eip" "proj-eip" {
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Project1" {
- ami = "ami-003932de22c285676"
- instance_type = "t2.medium"
+resource "aws_instance" "Project" {
+ ami = ami-0862be96e41dcbf74
+ instance_type = "t2.micro"
  availability_zone = "us-east-2"
  key_name = "roydemo"
  network_interface {
