e7a164d7e6a57f79a5badb668736b6a961f319fd
Vanita Roy <49209283+vanita6070@users.noreply.github.com>
2024-07-19 18:10:04 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -126,7 +126,7 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
+ ami = "ami-003932de22c285676"
  instance_type = "t2.micro"
  availability_zone = "us-east-2"
  key_name = "Tomcat"
